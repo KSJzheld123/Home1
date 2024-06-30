76f0db48de907743ee91cbb129bdbf5466189887
ksjzheld123 <rlatjswnzheld970616@gmail.com>
2024-06-30 18:37:03 +0900

    0630

---
+++ src/test/java/Tests.java
@@ -0,0 +1,85 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+import static org.assertj.core.api.Assertions.assertThat;
+
+public class Tests {
+
+    @Test
+    @DisplayName("1 + 1 == 2")
+    void t1() {
+        assertThat(Calc.run("1 + 1")).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("2 + 1 == 3")
+    void t2() {
+        assertThat(Calc.run("2 + 1")).isEqualTo(3);
+    }
+
+    @Test
+    @DisplayName("2 + 2 == 4")
+    void t3() {
+        assertThat(Calc.run("2 + 2")).isEqualTo(4);
+    }
+
+    @Test
+    @DisplayName("1000 + 200 == 1200")
+    void t4() {
+        assertThat(Calc.run("1000 + 200")).isEqualTo(1200);
+    }
+
+    @Test
+    @DisplayName("2 - 1 == 1")
+    void t5() {
+        assertThat(Calc.run("2 - 1")).isEqualTo(1);
+    }
+
+    @Test
+    @DisplayName("3 - 1 == 2")
+    void t6() {
+        assertThat(Calc.run("3 - 1")).isEqualTo(2);
+    }
+
+    @Test
+    @DisplayName("100 - 20 == 80")
+    void t7() {
+        assertThat(Calc.run("100 - 20")).isEqualTo(80);
+    }
+
+    @Test
+    @DisplayName("10 + 20 + 30 == 60")
+    void t8() {
+        assertThat(Calc.run("10 + 20 + 30")).isEqualTo(60);
+    }
+
+    @Test
+    @DisplayName("10 - 20 + 30 == 20")
+    void t9() {
+        assertThat(Calc.run("10 - 20 + 30")).isEqualTo(20);
+    }
+
+    @Test
+    @DisplayName("10 - 10 - 10 - 10 + 10 + 10 - 10 = -10")
+    void t10() {
+        assertThat(Calc.run("10 - 10 - 10 - 10 + 10 + 10 - 10")).isEqualTo(-10);
+    }
+
+    @Test
+    @DisplayName("10 * 20 / 10 = 20")
+    void t11() {
+        assertThat(Calc.run("10 * 20 / 10")).isEqualTo(20);
+    }
+
+    @Test
+    @DisplayName("20 + 30 - 50 * 20 + 10 / 10 - 10 = -959")
+    void t12() {
+        assertThat(Calc.run("20 + 30 - 50 * 20 + 10 / 10 - 10")).isEqualTo(-959);
+    }
+
+    @Test
+    @DisplayName("(10 + 20) == 30")
+    void t13() {
+        assertThat(Calc.run("(10 + 20)")).isEqualTo(30);
+    }
+}
