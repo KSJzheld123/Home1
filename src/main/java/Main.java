a56da8f7d5a8bfc50e5d43edbbce65854c044d2c
ksjzheld123 <rlatjswnzheld970616@gmail.com>
2024-08-10 21:04:51 +0900

    d20240810

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,5 +1,18 @@
-public class Main {
-    public static void main(String[] args) {
+class Solution {
+    public int[] solution(int[] num_list) {
 
+        int a =  0;
+        int b = 0;
+
+        for(int i = 0; i < num_list.length; i++){
+            if(num_list[i] % 2 == 0){
+                a++;
+            } else if (num_list[i] % 2 == 1) {
+                b++;
+            }
+        }
+
+        int[] answer = {a,b};
+        return answer;
     }
 }
